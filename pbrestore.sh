bedf0ac76ecbc80c00a21fa9af91364159546d6e
blastmaster <oeste.sebastian@googlemail.com>
2013-06-23 02:48:40 +0200

    understanding version names

--- pbrestore.sh
+++ pbrestore.sh
@@ -17,20 +17,29 @@ for ((i = 0; i <= ${#output[@]}; i++)) do
     fi
 done
 
+i=1
+for version in ${output[@]}; do
+    [[ ! -z $1 ]] && [[ $1 =~ "$version" ]] && src=$i
+    [[ ! -z $2 ]] && [[ $2 =~ "$version" ]] && target=$i
+    i=$(($i + 1))
+done
+
 tmpsrc=$(($current - 1))
 src=${1:-${tmpsrc}}
 [[ $src =~ "[0-9]+$" ]] && [[ ! -z ${output[$src]} ]] || help
 target=${2:-${current}}
 [[ $target =~ "[0-9]+$" ]]  && [[ ! -z ${output[$target]} ]] || help
 
+echo -n "newest "
+echo ${output[$newest]}
 echo -n "source "
 echo ${output[$src]}
 echo -n "target "
 echo ${output[$target]}
 
-perlbrew switch ${output[src]} > /dev/null
-perl -MExtUtils::Installed -E 'say for ExtUtils::Installed->new->modules' > /tmp/installed.list
-perlbrew switch ${output[$target]} > /dev/null
+# perlbrew switch ${output[src]} > /dev/null
+# perl -MExtUtils::Installed -E 'say for ExtUtils::Installed->new->modules' > /tmp/installed.list
+# perlbrew switch ${output[$target]} > /dev/null
 
 count=$(wc -l /tmp/installed.list | awk -F " " '{print $1}')
 echo "Install $count Modules yet? [y/n]"
