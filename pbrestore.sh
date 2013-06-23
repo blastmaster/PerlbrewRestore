8fe6dd138c752ea484afc66b79a5df4c098c8cb1
blastmaster <oeste.sebastian@googlemail.com>
2013-06-23 02:13:00 +0200

    first stage finished

---
+++ pbrestore.sh
@@ -0,0 +1,42 @@
+#! /usr/bin/zsh
+
+function help()
+{
+    echo "PerlbrewRestore [src] [target]"
+    echo "Install all Modules from src in perl version target"
+    exit
+}
+
+declare -a output
+output=($(perlbrew list | sed 's/^*\?\(.*\)/\1/'))
+newest=${#output[@]}
+
+for ((i = 0; i <= ${#output[@]}; i++)) do
+    if  [[ $PERLBREW_PERL == ${output[i]} ]]; then
+        current=$i
+    fi
+done
+
+tmpsrc=$(($current - 1))
+src=${1:-${tmpsrc}}
+[[ $src =~ "[0-9]+$" ]] && [[ ! -z ${output[$src]} ]] || help
+target=${2:-${current}}
+[[ $target =~ "[0-9]+$" ]]  && [[ ! -z ${output[$target]} ]] || help
+
+echo -n "source "
+echo ${output[$src]}
+echo -n "target "
+echo ${output[$target]}
+
+perlbrew switch ${output[src]} > /dev/null
+perl -MExtUtils::Installed -E 'say for ExtUtils::Installed->new->modules' > /tmp/installed.list
+perlbrew switch ${output[$target]} > /dev/null
+
+count=$(wc -l /tmp/installed.list | awk -F " " '{print $1}')
+echo "Install $count Modules yet? [y/n]"
+read x;
+case $x in
+    [yY]) echo "installing" cat /tmp/installed.list | cpanm --interactive ;;
+    [nN]) echo "Nothing" ;;
+    *) echo "invalid input Abort";;
+esac
